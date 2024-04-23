7a4bbd5e9654bd65a458f9d1c2f98ebb610eb7f8
b1ngusbangus <jacquelineemvo@berkeley.edu>
2024-04-22 22:31:22 -0700

    finished wdd final spring '24

---
+++ final/assets/scripts/script.js
@@ -0,0 +1,17 @@
+function moveCat() {
+
+    var image = document.getElementById('movingCat');
+    image.classList.add('animation');
+
+    // var image = document.getElementById('movingImage')
+    // var container = document.getElementById('container');
+    // var containerWidth = container.offsetWidth;
+    // var containerHeight = container.offsetHeight;
+
+    // var newX = Math.random() * (containerWidth - image.width);
+    // var newY = Math.random() * (containerHeight - image.height);
+
+    // image.style.transform = "translate(" + newX + "px, " + newY + "px)";
+}
+
+
